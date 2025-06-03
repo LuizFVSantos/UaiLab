78d76c337c604eb390d3917c67c5c379b4f93e0c
Luiz F V Santos <lfvsantos.bh@gmail.com>
2025-06-02 21:05:46 -0300

    org

---
+++ FrontEnd/jsFront.js
@@ -0,0 +1,26 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const elements = document.querySelectorAll(".elements");
+const maxElements = elements.length;
+
+controls.forEach(control => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains("arrow-left");
+        if (isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+        if (currentItem >= maxElements) {
+            currentItem = 0;
+        }
+        if (currentItem < 0) {
+            currentItem = maxElements - 1;
+        }
+        elements[currentItem].scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "start"
+        });
+    });
+});
